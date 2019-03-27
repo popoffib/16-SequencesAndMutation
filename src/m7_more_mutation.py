013decbe4cade8447c5c8dfc8aea3d86dea26f50
popoffib <popoffib@rose-hulman.edu>
2019-03-27 15:31:21 -0400

    done!!

--- src/m7_more_mutation.py
+++ src/m7_more_mutation.py
@@ -305,11 +305,13 @@ def MUTATE_circle(circle, color, n):
     Precondition: The first argument is a rg.Circle and the second
                   argument is a color appropriate for Zellegraphics.
     """
-    # TODO: 3. First, READ THE ABOVE TEST CODE.
+    # DONE: 3. First, READ THE ABOVE TEST CODE.
     #          Make sure that you understand it.
     #          Then, IMPLEMENT and test THIS FUNCTION
     #          (using the above code for testing).
     # HINT: This function can be implemented with just TWO lines of code.
+    circle.fill_color = color
+    circle.center.x = circle.center.x + n
 
 
 # ----------------------------------------------------------------------
